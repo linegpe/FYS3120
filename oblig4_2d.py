1609f54d32f4f1f2d1d15a91ed0c931b50e9c52b
linegpe <line.gaard.pedersen@gmail.com>
2017-02-16 14:03:00 +0100

    Added potential

--- oblig4_2d.py
+++ oblig4_2d.py
@@ -1,3 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+
+# Defining constants
+g = 1
+R = 1
+omega_er = np.sqrt(g/R)
+omega = [0.5,1.5]
+N = 100
+theta = np.linspace(-np.pi,np.pi,N)
+m = 1
+#potential = np.zeros(N)
+
+def potential(theta,omega):
+	return m*g*R*np.cos(theta)-0.5*m*omega**2*R**2*(np.sin(theta))**2
+
+#plt.plot()
 
